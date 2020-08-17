5be8fee4c5f658319b0f71a72c96a317ee7867a8
Son <Son@DESKTOP-N14RO5B>
2020-08-17 17:57:26 +0700

    UWP Tutorial 04

---
+++ DataBindingSample/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using DataBindingSample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingSample.Models
+{
+    class TodoItem
+    {
+        public String Title { get; set; }
+    }
+}
